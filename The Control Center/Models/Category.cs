6e62a8c098e4d6b541aa31714be8f1c224e778bc
Will Tham <tham.will00@gmail.com>
2023-11-05 13:51:54 -0800

    Grid Changes and some styling changes as well

--- The Control Center/Models/Category.cs
+++ The Control Center/Models/Category.cs
@@ -17,5 +17,12 @@ namespace The_Control_Center.Models
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; }= "Expense";
 
+        [NotMapped]
+        public string? TitleWithIcon {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        } 
     }
 }
